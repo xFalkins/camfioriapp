014dbb6f5a314c6ed6f71f2070fb3e54c2628fd0
xFalkins <87199207+xFalkins@users.noreply.github.com>
2021-09-17 03:55:11 +0200

    Update Eingabemaske.controller.js

--- app/webapp/controller/Eingabemaske.controller.js
+++ app/webapp/controller/Eingabemaske.controller.js
@@ -12,7 +12,73 @@ sap.ui.define([
         /* =========================================================== /
         / lifecycle methods                                           /
         / =========================================================== */
+ 	sendCAM : function() {
+            var name = this.getView().byId("input_name").getValue();
+			var kundennr = this.getView().byId("input_kundennr").getValue();
+            var limithoehe = this.getView().byId("input_limithoehe").getValue();
+            var befristung = this.getView().byId("picker_befristung").getValue();
+            var bemerkung = this.getView().byId("area_bemerkung").getValue();
+			var rating;
+			var status;
+			
+			var kundennummerStart = kundennr.charAt(0);
 
+			if(kundennr == "" || limithoehe == "" || befristung == "")
+			{
+				alert("Bitte füllen Sie alle Felder aus!");
+			}
+			else if(isNaN(kundennr))
+			{
+				alert("Bitte überprüfen Sie die Eingabe!");
+			}
+			else{
+				// Limit voll genehmigt
+				if( kundennummerStart == '0' || kundennummerStart == '1' || kundennummerStart == '2' )
+				{
+					status = "genehmigt";
+					rating = "BB+"
+				}
+				// Limit zur Hälfte genehmigt
+				else if( kundennummerStart == '3' || kundennummerStart == '4' || kundennummerStart == '5' )
+				{
+					status = "genehmigt";
+					limithoehe = Long.parseLong(limithoehe) / 2;
+					rating = "C-"
+				}
+				// Limit in Bearbeitung
+				else if( kundennummerStart == '6' || kundennummerStart == '7' )
+				{
+					status = "in Bearbeitung";
+					limithoehe = 0;
+					rating = "B+"
+				}
+				// Limit abgelehnt
+				else if( kundennummerStart == '8' || kundennummerStart == '9' )
+				{
+					status = "abgelehnt";
+					limithoehe = 0;
+					rating = "D"
+				}
+				// Limit voll genehmigt mit AA+
+				else
+				{
+					Status = "genehmigt";
+					Limit = Limit;
+					rating = "AA+"
+				} 
+
+				var testJSON = JSON.stringify([rating, befristung, limithoehe, bemerkung, status, kundennr]);
+
+				
+				//Bestätigung
+				alert("Anfrage erfolgreich an CAM gesendet!");
+
+				// Senden an Worklist + Reload
+				var oRouter=sap.ui.core.UIComponent.getRouterFor(this);             
+				oRouter.navTo("worklist");
+				location.reload();
+			}
+        },
         onNavBack : function() {
             history.go(-1);
             
